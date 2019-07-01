2b2ceac731897b7dac7793a28c7b26090cb6ab56
MarcoFavorito <marco.favorito@gmail.com>
2019-07-01 09:17:22 +0100

    update script to generate private key

--- scripts/generate_private_key.py
+++ scripts/generate_private_key.py
@@ -3,13 +3,14 @@
 
 """
 Generate a private key to be used for the Trading Agent Competition.
-It prints the key in PEM format to stdout.
+It prints the key in PEM format to the specified file.
 """
+
 from tac.helpers.crypto import Crypto
 from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
 
 import argparse
-parser = argparse.ArgumentParser("generate_private_key")
+parser = argparse.ArgumentParser("generate_private_key", description=__doc__)
 parser.add_argument("out_file", type=str, help="Where to save the private key.")
 
 if __name__ == '__main__':
