ec15c9cdd1f074f0bd57093c0cc285950828e1ca
MarcoFavorito <marco.favorito@gmail.com>
2019-07-01 09:17:03 +0100

    update oef healthcheck script

--- sandbox/oef_healthcheck.py
+++ sandbox/oef_healthcheck.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""This script waits until the OEF is up and running"""
+
+import argparse
 import asyncio
 
 import oef.agents
 
-import sys
+parser = argparse.ArgumentParser("oef_healthcheck", description=__doc__)
+parser.add_argument("addr", type=str, help="IP address of the OEF node.")
+parser.add_argument("port", type=int, help="Port of the OEF node.")
 
 if __name__ == '__main__':
     try:
-        host = sys.argv[1]
-        port = sys.argv[2]
+        args = parser.parse_args()
+        host = args.addr
+        port = args.port
         pbk = 'check'
         print("Connecting to {}:{}".format(host, port))
         agent = oef.agents.OEFAgent(pbk, host, port, loop=asyncio.get_event_loop())
