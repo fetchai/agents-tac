a38ae73f6e81771949bcbce48133450e83adfce5
David Minarsch <davidminarsch@Davids-MacBook-Pro-3.local>
2019-08-05 14:20:18 +0100

    adds gh-pages publication script

---
+++ docs/publish_to_ghpages.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+if [ -z $1 ]
+then
+	echo "Please provide a tag argument."
+    exit 1;
+fi
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+set -e
+
+echo "Deleting old publication"
+rm -rf _build
+
+echo "Building docs"
+sphinx-apidoc -o reference/api/ ../tac/
+make html
+cd ..
+
+echo "Create a local gh-pages branch containing the splitted output folder"
+git subtree split --prefix docs/_build -b gh-pages
+
+echo "Pushing to gh-pages branch"
+git add --all && git commit -m "Publishing to gh-pages with tag $1"
+git tag $1
+git pull origin gh-pages
+git push origin gh-pages:gh-pages
+
+
+echo "Delete local gh-pages"
+git branch -D gh-pages
