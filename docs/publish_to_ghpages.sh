3e4ce5b6550852edc7336a63967a880da916a679
David Minarsch <davidminarsch@Davids-MacBook-Pro-3.local>
2019-08-05 15:04:30 +0100

    Updates deploy script

--- docs/publish_to_ghpages.sh
+++ docs/publish_to_ghpages.sh
@@ -5,7 +5,10 @@ then
 	echo "Please provide a tag argument."
     exit 1;
 fi
-if [[ $(git status -s) ]]
+
+STATUS="$(git status)"
+
+if ! [[ $STATUS == *"nothing to commit, working directory clean"* ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
     exit 1;
@@ -22,14 +25,18 @@ make html
 cd ..
 
 echo "Create a local gh-pages branch containing the splitted output folder"
+sed -i "" 'docs/_build/' ./.gitignore
+git add .
+git commit -m "Edit .gitignore to publish"
 git subtree split --prefix docs/_build -b gh-pages
+git reset HEAD~
+git checkout .gitignore
 
 echo "Pushing to gh-pages branch"
 git add --all && git commit -m "Publishing to gh-pages with tag $1"
 git tag $1
 git pull origin gh-pages
 git push origin gh-pages:gh-pages
-
 
 echo "Delete local gh-pages"
 git branch -D gh-pages
