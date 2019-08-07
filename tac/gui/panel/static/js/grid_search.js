db93689e9716e387fd865a1c6b1126556d9cc3e6
MarcoFavorito <marco.favorito@gmail.com>
2019-08-07 10:57:20 +0100

    implement onclick action for the button to remove the item.

--- tac/gui/panel/static/js/grid_search.js
+++ tac/gui/panel/static/js/grid_search.js
@@ -1,22 +1,53 @@
 (function() {
 
-    let card_html_template = $("#card-1").html();
+    let firstCard = $("#card-0");
+    let cardHtmlTemplate = firstCard.html();
+    firstCard.find("button[name='btn-remove-item']").on('click', function(){
+        firstCard.remove();
+    });
+
     let sandboxList = $("#accordion");
+    let nbCards = 1;
 
     let createSandboxCard = function(id){
         let card = $("<div></div>");
-        card.html(card_html_template
-            .replace(/collapse-1/g, "collapse-" + id)
-            .replace(/heading-1/g, "heading-"+id)
-            .replace(/Sandbox 1/, "Sandbox " + id));
+        card.html(cardHtmlTemplate
+            .replace(/collapse-0/g, "collapse-" + id)
+            .replace(/heading-0/g, "heading-"+id)
+            .replace(/Sandbox 1/, "Sandbox " + (id + 1)));
         card.addClass("card");
         card.attr("id", "card-" + id);
+
+        //remove card function
+        card.find("button[name='btn-remove-item']").on('click', function(){
+            card.remove();
+        });
         return card;
     };
 
+    let buildJSONFromSandboxFormList = function(){
+        let result = [];
+        let nbCards = sandboxList.children().length;
+        for (let i = 0; i < nbCards; i++){
+            let inputs = $('#card-' + i).find('.card-body :input');
+            let values = {};
+            inputs.each(function() {
+                values[this.name] = $(this).val();
+            });
+            result.push(values);
+        }
+        return result
+    };
+
     $("#btn-add-sandbox").on("click", function(){
-        let card = createSandboxCard(sandboxList.children().length + 1);
+        let card = createSandboxCard(nbCards);
         sandboxList.append(card);
+        nbCards += 1;
+    });
+
+    $("#btn-submit-gridsearch").on("click", function(){
+        let sandboxObjectList = buildJSONFromSandboxFormList();
+        console.log(sandboxObjectList)
     });
 
 
